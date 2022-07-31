0198c276245dfc022f7efbabc8e58eb6158e5679
Leidyy <98460700+LeidyyTR@users.noreply.github.com>
2022-07-31 22:10:38 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -30,7 +30,7 @@ I am a professional editor and intermediate software developer. I have been prof
                 </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="LeidyyTR" src={`https://media.discordapp.net/attachments/737348411568685066/996841172372824184/YT_AVATAR_NET.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <img alt="LeidyyTR" src={`https://cdn.discordapp.com/avatars/850844135967031316/a_9261dcfeb796915ded6f587fd436160c.gif?size=1024`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
                 <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
                   <Tippy content="Online" animation="shift-away" arrow={false}>
                     <i className={`fad fa-circle text-2xl text-online`} />
